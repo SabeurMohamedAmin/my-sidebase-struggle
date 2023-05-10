7100578dbc33cc7ef466251248dd303856287ea0
Cristian Casallas <cristiancasallas998@gmail.com>
2023-05-10 16:16:43 -0500

    test of content in new post

--- pages/guest.vue
+++ pages/guest.vue
@@ -10,14 +10,20 @@ definePageMeta({
 const route = useRoute()
 const { $client } = useNuxtApp()
 
-const { data: hello } = await $client.hello.hello.useQuery()
+const { data: hello } = await $client.hello.hello.useQuery({
+  text: 'finally this is the hello subroute from trpc'
+})
+
+const { data: getAll } = $client.posts.getAll.useQuery()
+console.log(getAll.value)
 </script>
 <template>
   <div>
     Hey I'm only for guests
     <p>{{ hello?.greeting }}</p>
-    <pre>
-      {{ getAll }}
-    </pre>
+
+    <div v-for="post in getAll" class="">
+      <p>{{ post.content }}</p>
+    </div>
   </div>
 </template>
